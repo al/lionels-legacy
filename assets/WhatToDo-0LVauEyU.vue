9ff1d3089c2efa02291ae19615c8921ae16393f0
Alan Larkin <alan@courtsdesk.com>
2024-06-25 00:48:40 +0100

    Updates

---
+++ assets/WhatToDo-0LVauEyU.vue
@@ -0,0 +1,171 @@
+<template>
+  <div
+    class="md:text-center mx-auto px-4 pt-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:pt-20"
+  >
+    <h2
+      class="my-20 mt-12 max-w-full scroll-mt-[80px] font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto"
+      id="what-to-do"
+    >
+      If you are involved in or witness an accident involving a cat you must act
+    </h2>
+
+    <p class="my-20 text-base text-gray-700 md:text-lg">
+      Failure to do so is punishable by a fine of
+      <strong class="text-red-accent-700">up to £10k</strong> or up to
+      <strong class="text-red-accent-700">6 month's imprisonment</strong>
+    </p>
+  </div>
+
+  <div class="row-gap-10 mx-auto my-20 grid max-w-screen-lg gap-8 md:grid-cols-3">
+    <div class="flex max-w-md sm:flex-row">
+      <div class="mr-4">
+        <div class="mb-4 flex h-12 w-12 items-center justify-center rounded-full bg-indigo-50">
+          <svg
+            class="h-6 w-6 text-orange-accent-400"
+            fill="currentColor"
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+            <path
+              d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"
+            />
+          </svg>
+        </div>
+      </div>
+      <div>
+        <h6 class="mb-3 text-xl font-bold leading-5">Stop</h6>
+        <p class="mb-3 text-sm text-gray-900">Stop your vehicle as soon as it is safe to do so</p>
+      </div>
+    </div>
+    <div class="flex max-w-md sm:flex-row">
+      <div class="mr-4">
+        <div class="mb-4 flex h-12 w-12 items-center justify-center rounded-full bg-indigo-50">
+          <svg
+            class="h-6 w-6 text-orange-accent-400"
+            fill="currentColor"
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+            <path
+              d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"
+            />
+          </svg>
+        </div>
+      </div>
+      <div>
+        <h6 class="mb-3 text-xl font-bold leading-5">Check</h6>
+        <p class="mb-3 text-sm text-gray-900">Check the animal for signs of injuries</p>
+      </div>
+    </div>
+    <div class="flex max-w-md sm:flex-row">
+      <div class="mr-4">
+        <div class="mb-4 flex h-12 w-12 items-center justify-center rounded-full bg-indigo-50">
+          <svg
+            class="h-6 w-6 text-orange-accent-400"
+            fill="currentColor"
+            viewBox="0 0 512 512"
+          >
+            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+            <path
+              d="M280 0C408.1 0 512 103.9 512 232c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-101.6-82.4-184-184-184c-13.3 0-24-10.7-24-24s10.7-24 24-24zm8 192a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm-32-72c0-13.3 10.7-24 24-24c75.1 0 136 60.9 136 136c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-48.6-39.4-88-88-88c-13.3 0-24-10.7-24-24zM117.5 1.4c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L144 207.3c33.3 70.4 90.3 127.4 160.7 160.7L345 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C481.8 499.9 466 512 448 512C200.6 512 0 311.4 0 64C0 46 12.1 30.2 29.5 25.4l88-24z"
+            />
+          </svg>
+        </div>
+      </div>
+      <div>
+        <h6 class="mb-3 text-xl font-bold leading-5">Report</h6>
+        <p class="mb-3 text-sm text-gray-900">
+          Report the incident to the owner or call the
+          <a
+            class="inline-flex items-center font-semibold text-orange-accent-400 transition-colors duration-200 hover:text-orange-900"
+            aria-label=""
+            href="https://jspca.org.je/"
+            >JSPCA</a
+          >
+          on
+          <a
+            class="inline-flex items-center font-semibold text-orange-accent-400 transition-colors duration-200 hover:text-orange-900"
+            aria-label="Call the JSPCA's phone number"
+            href="tel:01534724331"
+            >01534 724 331</a
+          >
+          <span class="md:hidden lg:inline">
+            (or
+            <a
+              class="inline-flex items-center font-semibold text-orange-accent-400 transition-colors duration-200 hover:text-orange-900"
+              aria-label="Call the JSPCA's phone number"
+              href="tel:07797720331"
+              >07797 720 331</a
+            >
+            out of hours)
+          </span>
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="vvv">
+    <div
+      class="mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20"
+    >
+      <div class="max-w-xl sm:text-center md:mx-auto md:text-left lg:max-w-2xl">
+        <div class="flex flex-col md:flex-row">
+          <h2
+            class="word-break mb-6 max-w-full flex-1 font-sans text-3xl font-bold tracking-tight text-gray-500 sm:text-4xl md:mx-auto md:w-1/4 md:flex-none md:leading-relaxed"
+          >
+            Do the right thing
+          </h2>
+          <ul class="flex-1 list-disc text-left md:w-3/4">
+            <li class="mb-6 text-base text-gray-700 md:text-lg">
+              Remember that for many pets are like family members. If an injured cat receives help
+              early, it is possible that their life could be saved.
+            </li>
+
+            <li class="mb-6 text-base text-gray-700 md:text-lg">
+              For your own wellbeing it is better to report the incident yourself rather than
+              disregard the law and risk having your actions reported to the
+              <a
+                class="inline-flex items-center font-semibold text-orange-accent-400 transition-colors duration-200 hover:text-orange-900"
+                aria-label=""
+                href="https://jersey.police.uk/"
+                >States of Jersey Police</a
+              >
+              or
+              <a
+                class="inline-flex items-center font-semibold text-orange-accent-400 transition-colors duration-200 hover:text-orange-900"
+                aria-label=""
+                href="https://jspca.org.je/"
+                >JSPCA</a
+              >
+              by witnesses.
+            </li>
+            <li class="text-base text-gray-700 md:text-lg">
+              Remember that many properties now have road facing private cameras. Unfortunately for
+              Lionel on 4 June 2024, the day he was killed, the camera that would have caught the
+              incident was offline. The driver did not report hitting and killing Lionel with their
+              vehicle and left the scene, thereby breaking the law. Had the camera been on that day,
+              the driver would have been recorded and their actions would be undeniable.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+@media screen(md) {
+  .word-break {
+    word-spacing: 9999999px;
+  }
+}
+
+.vvv {
+  background-color: #f8fafc;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23d4e0eb' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+}
+</style>
