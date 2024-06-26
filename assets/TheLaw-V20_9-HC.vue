86d9d99594d2fd9bc51ba2962eba639af6f4066e
Alan Larkin <alan@courtsdesk.com>
2024-06-26 10:36:13 +0100

    Updates

---
+++ assets/TheLaw-V20_9-HC.vue
@@ -0,0 +1,119 @@
+<template>
+  <div
+    class="mx-auto px-4 pt-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:pt-20"
+  >
+    <div class="mb-10 max-w-xl sm:text-center md:mx-auto md:mb-12 lg:max-w-2xl">
+      <h2
+        class="mb-6 max-w-full scroll-mt-[80px] text-3xl font-bold leading-none tracking-tight text-gray-800 sm:text-4xl md:mx-auto"
+        id="the-law"
+      >
+        <span class="relative inline-block">
+          <svg
+            class="absolute left-0 top-0 z-0 -ml-20 -mt-8 hidden w-32 text-blue-gray-100 sm:block lg:-ml-28 lg:-mt-10 lg:w-32"
+            fill="currentColor"
+            viewBox="0 0 52 24"
+          >
+            <defs>
+              <pattern
+                id="27df4f81-c854-45de-942a-fe90f7a300f9"
+                height=".30"
+                width=".135"
+                x="0"
+                y="0"
+              >
+                <circle
+                  cx="1"
+                  cy="1"
+                  r=".7"
+                ></circle>
+              </pattern>
+            </defs>
+            <rect
+              fill="url(#27df4f81-c854-45de-942a-fe90f7a300f9)"
+              height="24"
+              width="52"
+            ></rect>
+          </svg>
+          <span class="relative">Jersey&rsquo;s</span>
+        </span>
+        road traffic law has changed
+      </h2>
+      <p class="mb-12 text-base text-gray-700 md:text-lg">
+        As a motorist you now have explicit duties if your car hits a cat.
+      </p>
+
+      <p class="mb-12 text-base text-gray-700 md:text-lg">
+        Under the updated regulations, the driver of a motor vehicle on a public road who thinks he
+        or she has hit or run over a cat must do two things:
+      </p>
+      <ol
+        class="marker:text-orange-accent-400 mb-12 px-10 text-base text-gray-700 md:text-lg list-decimal"
+      >
+        <li class="mb-2">
+          The driver must stop &ldquo;as soon as it is safe and reasonably practicable to do
+          so&rdquo;; and
+        </li>
+        <li>
+          The driver must notify either a person who is responsible for caring for the cat or the
+          JSPCA.
+        </li>
+      </ol>
+
+      <figure class="md:text-l mb-12 px-10 text-left text-gray-800">
+        <blockquote
+          class="leading-loose border-1 py-3 border-y"
+          cite="https://www.jerseylaw.je/laws/current/Pages/25.550.aspx#_Toc151627514"
+        >
+          <ol
+            class="list-decimal font-serif"
+            start="52"
+          >
+            <li class="pl-2">
+              <span class="font-semibold">Duty of driver of vehicle in case of accident</span>
+
+              <ol
+                class="list-decimal pl-5"
+                start="4"
+              >
+                <li class="pl-2">
+                  Subject to paragraph (5), if a cat is hit by a motor vehicle while that vehicle is
+                  being driven on a road, the driver of the vehicle must –
+
+                  <ol class="list-latin pl-5">
+                    <li class="pl-2">
+                      stop the vehicle as soon as it is safe or reasonably practicable to do so; and
+                    </li>
+                    <li class="pl-2">
+                      notify a person or body described in paragraph (6) of –
+
+                      <ol class="list-roman pl-5">
+                        <li class="pl-2">when the cat was hit, and</li>
+                        <li class="pl-2">
+                          the location where the cat was last seen by the driver.
+                        </li>
+                      </ol>
+                    </li>
+                  </ol>
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </blockquote>
+        <cite class="block text-right pt-3"
+          >Article 52(4) of the
+          <a
+            class="text-orange-accent-400 transition-colors duration-200 hover:text-orange-900"
+            aria-label=""
+            href="https://www.jerseylaw.je/laws/current/Pages/25.550.aspx#_Toc151627514"
+            target="_blank"
+            >Road Traffic (Jersey) Law 1956
+            <ArrowTopRightOnSquareIcon class="w-3 inline-block pb-2" /> </a
+        ></cite>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/20/solid'
+</script>
