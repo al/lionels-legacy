698fbbd5359a69b1e0658f78353c1d6f063c634d
Alan Larkin <alan.larkin@gmail.com>
2024-07-22 14:29:06 +0100

    feat: Google Tag Manager
    Add Google Tag Manager to the application whenever a `VITE_GTM_ID`
    environment variable is detected.
    
    In live environments `VITE_GTM_ID` is expected to be set via secrets,
    but in other environments you can use the .env.* files.
    
    LINKS:
    
    - https://tagmanager.google.com/#/home

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '/src/assets/stylesheets/index.css'
 
+import { createGtm } from '@gtm-support/vue-gtm'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +8,15 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+
+const gtmId = import.meta.env.VITE_GTM_ID
+if (gtmId) {
+  app.use(
+    createGtm({
+      id: gtmId,
+      vueRouter: router,
+    })
+  )
+}
 
 app.mount('#app')
