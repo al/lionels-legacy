0c005e53bd5928bd1f5ae7d867a325aef0f7ff1b
Alan Larkin <alan.larkin@gmail.com>
2024-07-22 14:28:59 +0100

    core: Linters
    Add:
    - ESLint
    - Prettier
    - CSpell
    - StyleLint
    - lint-staged
    
    A Husky pre-commit hook will run all linters on all staged files (lint-
    staged).
    
    To manually run all linters:
        pnpm run lint
        pnpm run lint:fix
    
    To manually run Javascript linters (ESLint):
        pnpm run lint:js
        pnpm run lint:js:fix
    
    To manually run Javascript style linters (Prettier):
        pnpm run format
        pnpm run format:fix
    
    To manually run spellchecker (CSpell):
        pnpm run spelling
        pnpm run spelling:fix
    
    To manually run Stylesheet lintets (Stylelint):
        pnpm run lint:css
        pnpm run lint:css:fix

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Developers
 
+### Setup
+
 Register the shared Git configuration file ([.gitconfig](.gitconfig)):
 
 ```sh
@@ -20,6 +22,53 @@ Install Node dependencies, including the requisite version of Node if necessary 
 pnpm install
 ```
 
-Git hooks are managed by [Husky](https://typicode.github.io/husky/).
+### Lint with [ESLint](https://eslint.org/) and [Stylelint](https://stylelint.io/)
 
-Commit message linting is provided by [commitlint](https://github.com/conventional-changelog/commitlint?tab=readme-ov-file). Messages should conform to the [Conventional Commit](https://www.conventionalcommits.org/en/v1.0.0/) specification.
+```sh
+pnpm lint
+```
+
+Or fix lints:
+
+```sh
+pnpm lint:fix
+```
+
+For more targetted linting, the following are also available:
+
+```sh
+pnpm lint:js
+pnpm lint:js:fix
+pnpm lint:css
+pnpm lint:css:fix
+```
+
+### Format with [Prettier](https://prettier.io/)
+
+```sh
+pnpm format
+```
+
+Or fix formatting issues:
+
+```sh
+pnpm format:fix
+```
+
+### Spellcheck with [CSpell](https://cspell.org/)
+
+```sh
+pnpm spelling
+```
+
+Or launch an [interactive script](sbin/cspell-i) to add unrecognised words to the list of `ignoreWords` in [cspell.config.yaml](cspell.config.yaml):
+
+```sh
+pnpm spelling:fix
+```
+
+### Other Notable Tools
+
+- Git hooks are managed by [Husky](https://typicode.github.io/husky/).
+- Commit message linting is provided by [commitlint](https://github.com/conventional-changelog/commitlint?tab=readme-ov-file). Messages should conform to the [Conventional Commit](https://www.conventionalcommits.org/en/v1.0.0/) specification.
+- [lint-staged](https://github.com/lint-staged/lint-staged) ensures that pre-commit checks (linting, formatting, and spell checking) are only run on staged files.
---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import { includeIgnoreFile } from '@eslint/compat'
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginJson from 'eslint-plugin-json'
+import eslintPluginMarkdown from 'eslint-plugin-markdown'
+import eslintPluginYaml from 'eslint-plugin-yml'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+/**
+ * Helper function to process ignore files
+ *
+ * There seems to be a problem with using `includeIgnoreFile` from @eslint/compat directly in flat
+ * config files. The presence of additional properties (specifically `name`) in the response appears
+ * to prevent the `ignores` from working. This logic simply strips out all properties other than
+ * `ignores`, which seems to work.
+ *
+ * It also handles the conversion of the supplied argument to an absolute path.
+ */
+const includeIgnoreFileHelper = (file) => {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  const { ignores } = includeIgnoreFile(path.resolve(__dirname, file))
+
+  return { ignores }
+}
+
+export default [
+  includeIgnoreFileHelper('.gitignore'),
+  includeIgnoreFileHelper('.gitignore.local'),
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginJson.configs['recommended'],
+  ...eslintPluginYaml.configs['flat/recommended'],
+  {
+    files: ['**/*.md'],
+    plugins: {
+      markdown: eslintPluginMarkdown,
+    },
+    processor: 'markdown/markdown',
+  },
+  {
+    files: ['**/*.md/*.js'],
+    rules: {
+      'import/no-unresolved': 'off',
+      'no-console': 'off',
+      'no-undef': 'off',
+      'no-unused-expressions': 'off',
+      'no-unused-vars': 'off',
+      'padded-blocks': 'off',
+    },
+  },
+]
