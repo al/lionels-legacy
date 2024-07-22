7f479e00a91c48a340a7fefd3cb714a7faf74f5d
Alan Larkin <alan.larkin@gmail.com>
2024-07-22 14:29:02 +0100

    feat: Vue Router scroll behaviour
    Update the Router to scroll to the section of the page identified by
    the hash property of the URL.

--- src/router/index.js
+++ src/router/index.js
@@ -16,9 +16,36 @@ const routes = [
   },
 ]
 
+const scrollBehavior = function (to, _from, savedPosition) {
+  if (to.hash) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const element = document.querySelector(to.hash)
+        if (element) {
+          const style = window.getComputedStyle(element)
+          const scrollMarginTop = parseInt(style.scrollMarginTop, 10) || 0
+
+          resolve({
+            behavior: 'smooth',
+            el: to.hash,
+            top: scrollMarginTop,
+          })
+        } else {
+          resolve({ left: 0, top: 0 })
+        }
+      }, 300)
+    })
+  } else if (savedPosition) {
+    return savedPosition
+  } else {
+    return { top: 0 }
+  }
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior,
 })
 
 export default router
