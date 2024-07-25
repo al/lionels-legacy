e8831d1d1675c217dad150660119b4102c37d248
Alan Larkin <alan.larkin@gmail.com>
2024-07-25 17:30:42 +0100

    core: Prerendering
    Integrate prerendering of pages into the standard build process (i.e.
    `pnpm build`) using vite-plugin-prerender.
    
    The prerendered pages will be output to the dist/ directory by default.
    
    NOTES
    
    - On Apple devices using Apple Silicon it may be necessary to set the
      `PUPPETEER_EXECUTABLE_PATH` environment variable and use it to point
      to a working installation of a version of Chromium that is compatible
      with installed version of Puppeteer. The Puppeteer post-install
      scripts ought to install such a version of Chromium in ~/.cache. Use
      a .env.production.local file to do this.
    
    LINKS
    
    - https://github.com/Rudeus3Greyrat/vite-plugin-prerender
    - https://github.com/puppeteer/puppeteer

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import UnheadVite from '@unhead/addons/vite'
 import vue from '@vitejs/plugin-vue'
 import dotenv from 'dotenv-flow'
+import { createRequire } from 'module'
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+const require = createRequire(import.meta.url)
+const vitePluginPrerender = require('vite-plugin-prerender')
 
 dotenv.config()
 
@@ -14,10 +19,28 @@ for (const key in process.env) {
   }
 }
 
+/**
+ * NOTE: If encountering prerendering problems when running on an Apple M1 chip, see README.md for
+ *       possible workarounds.
+ */
+const prerenderOptions = {
+  renderer: new vitePluginPrerender.PuppeteerRenderer({
+    args: ['--no-sandbox '],
+    headless: true,
+    maxConcurrentRoutes: 1,
+    renderAfterDocumentEvent: 'render-complete',
+  }),
+  routes: ['/'], // TODO: Figure out how to set this dynamically.
+  staticDir: path.join(__dirname, 'dist/'),
+}
+
 export default defineConfig({
   base: '/',
+  build: {
+    assetsInlineLimit: 0,
+  },
   define: envVariables,
-  plugins: [UnheadVite(), vue(), vueDevTools()],
+  plugins: [UnheadVite(), vitePluginPrerender(prerenderOptions), vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
